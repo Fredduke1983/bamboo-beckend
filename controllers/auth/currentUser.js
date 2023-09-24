b2cc70024e870dac85d460344185e68822fe1349
Nechai_Ihor <fredduke1983@gmail.com>
2023-09-24 21:42:36 +0300

    ADD: getCurrent

--- controllers/auth/currentUser.js
+++ controllers/auth/currentUser.js
@@ -1,9 +1,20 @@
 const User = require("../../models/userSchema");
+const { ObjectId } = require("mongodb");
+const { errorsCatcher } = require("../../utils/errorsCatcher");
+const catchAsync = require("../../utils/catchAsync");
 
-const getCurrentUser = async (req, res) => {
-  console.log(req.params.id);
-  const current = await User.findById(req.params.id);
-  console.log(current);
-};
+const getCurrentUser = catchAsync(async (req, res) => {
+  const { id: paramsId } = req.params;
+  const isCorrectToken = ObjectId.isValid(paramsId);
+  if (!isCorrectToken) {
+    errorsCatcher(400, "incorrect id");
+  }
+
+  const current = await User.findById(paramsId).select("-password");
+  if (!current) {
+    errorsCatcher(404, "user not found");
+  }
+  res.status(200).json({ currentUser: current });
+});
 
 module.exports = getCurrentUser;
